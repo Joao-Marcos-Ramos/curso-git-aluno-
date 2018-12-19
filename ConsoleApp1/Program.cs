171663a9ebddc0155e1af60259f8dbdefdc3f2ea
Joao Marcos <jmramos625@gmail.com>
2018-12-19 15:36:30 -0800

    Primeira versao git

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+
+            Console.Write("Digite a frase");
+           
+
+            Console.WriteLine("A Frase é:" + frase);
+        }
+    }
+}
